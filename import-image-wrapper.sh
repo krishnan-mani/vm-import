3ba1bbd840c3a5ea4b1e447a3ea2de5be0e91966
Krishnan Mani <km@krishnanm.com>
2016-04-11 11:16:48 +0530

    No longer expecting km@a1

--- import-image-wrapper.sh
+++ import-image-wrapper.sh
@@ -4,13 +4,6 @@
 
 export SCRIPT_NAME="$0"
 
-if [ -z "$AWS_PROFILE" ]
-then
-	echo "Missing AWS_PROFILE: Set AWS_PROFILE to the user credential profile to use with aws-cli"
-	echo "Usage: $SCRIPT_NAME"
-	exit 1
-fi
-
 if [ $# -ne 3 ]
 then
   echo "Error: missing argument(s)"
